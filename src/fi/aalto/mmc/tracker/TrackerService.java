bb49211bb67144c91e950e27953d9d48ce6b7b33
Nicklas Overgaard <nicklas@isharp.dk>
2011-11-30 21:02:20 +0200

    Service now hooks into the location manager to receieve location updates

--- src/fi/aalto/mmc/tracker/TrackerService.java
+++ src/fi/aalto/mmc/tracker/TrackerService.java
@@ -4,8 +4,18 @@
 package fi.aalto.mmc.tracker;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Toast;
 
 /**
  * @author nover
@@ -14,13 +24,54 @@ import android.os.IBinder;
  */
 public class TrackerService extends Service {
 
-	/* (non-Javadoc)
-	 * @see android.app.Service#onBind(android.content.Intent)
-	 */
+	// Define a listener that responds to location updates
+	LocationListener locationListener = new LocationListener() {
+	    public void onLocationChanged(Location location) {
+
+	    	// Called when a new location is found by the network location provider.
+	    	
+	      // makeUseOfNewLocation(location);
+	    	
+
+	    }
+
+	    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+	    public void onProviderEnabled(String provider) {}
+
+	    public void onProviderDisabled(String provider) {}
+	  };
+	  
+	
+	
 	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	  public void onCreate() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+	    // Register the listener with the Location Manager to receive location updates every two minutes
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 60 * 2, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 60 * 2, 0, locationListener);
+	  }
+
+	  @Override
+	  public int onStartCommand(Intent intent, int flags, int startId) {
+	      Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+	      
+	      // If we get killed, after returning from here, restart
+	      return START_STICKY;
+	  }
+
+	  @Override
+	  public IBinder onBind(Intent intent) {
+	      // We don't provide binding, so return null
+	      return null;
+	  }
+	  
+	  @Override
+	  public void onDestroy() {
+	    Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show(); 
+	  }
 
 }
