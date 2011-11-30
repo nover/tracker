5d70b07bf8224f4df49cfc76b187e31e508ff6fc
Nicklas Overgaard <nicklas@isharp.dk>
2011-11-30 21:03:10 +0200

    The activity can now start the service

--- src/fi/aalto/mmc/tracker/TrackerActivity.java
+++ src/fi/aalto/mmc/tracker/TrackerActivity.java
@@ -1,7 +1,11 @@
 package fi.aalto.mmc.tracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TrackerActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,15 @@ public class TrackerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), TrackerService.class);
+				startService(intent);
+			}
+		});
+        
     }
 }
