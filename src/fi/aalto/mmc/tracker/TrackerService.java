89d0de7cc5d893142d385e44b3f82f22ef909a02
Nicklas Overgaard <nicklas@isharp.dk>
2011-11-30 21:26:00 +0200

    Eclipse reformatting

--- src/fi/aalto/mmc/tracker/TrackerService.java
+++ src/fi/aalto/mmc/tracker/TrackerService.java
@@ -20,59 +20,69 @@ import android.widget.Toast;
 /**
  * @author nover
  * 
- * The service for Tracker which hooks the android location services and stores information
+ *         The service for Tracker which hooks the android location services and
+ *         stores information
  */
 public class TrackerService extends Service {
 
 	// Define a listener that responds to location updates
 	LocationListener locationListener = new LocationListener() {
-	    public void onLocationChanged(Location location) {
+		public void onLocationChanged(Location location) {
 
-	    	// Called when a new location is found by the network location provider.
-	    	
-	      // makeUseOfNewLocation(location);
-	    	
-	    	Toast.makeText(getApplicationContext(), "Latitude " + location.getLatitude() + " longtitude: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+			// Called when a new location is found by the network location
+			// provider.
 
-	    }
+			// makeUseOfNewLocation(location);
 
-	    public void onStatusChanged(String provider, int status, Bundle extras) {}
+			Toast.makeText(
+					getApplicationContext(),
+					"Latitude " + location.getLatitude() + " longtitude: "
+							+ location.getLongitude(), Toast.LENGTH_SHORT)
+					.show();
 
-	    public void onProviderEnabled(String provider) {}
+		}
 
-	    public void onProviderDisabled(String provider) {}
-	  };
-	  
-	
-	
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		public void onProviderEnabled(String provider) {
+		}
+
+		public void onProviderDisabled(String provider) {
+		}
+	};
+
 	@Override
-	  public void onCreate() {
+	public void onCreate() {
 		// Acquire a reference to the system Location Manager
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		
-	    // Register the listener with the Location Manager to receive location updates every two minutes: 1000 * 60 * 2
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 , 0, locationListener);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-	  }
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
 
-	  @Override
-	  public int onStartCommand(Intent intent, int flags, int startId) {
-	      Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+		// Register the listener with the Location Manager to receive location
+		// updates every two minutes: 1000 * 60 * 2
+		locationManager.requestLocationUpdates(
+				LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				1000, 0, locationListener);
+	}
 
-	      
-	      // If we get killed, after returning from here, restart
-	      return START_STICKY;
-	  }
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
 
-	  @Override
-	  public IBinder onBind(Intent intent) {
-	      // We don't provide binding, so return null
-	      return null;
-	  }
-	  
-	  @Override
-	  public void onDestroy() {
-	    Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show(); 
-	  }
+		// If we get killed, after returning from here, restart
+		return START_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// We don't provide binding, so return null
+		return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+	}
 
 }
